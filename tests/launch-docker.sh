0c5dde532c159fc1196fe3b383572d8506c6640e
Johan Guldmyr <johan.guldmyr@csc.fi>
2017-01-05 08:09:17 +0200

    testing: cleanup and speedup

--- tests/launch-docker.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sudo docker build --rm -t local/c7-systemd -f tests/Dockerfile .
-sudo docker run -it --privileged=true --rm=true -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v `pwd`:/ansible-role-rdma:rw local/c7-systemd /bin/bash -c "/ansible-role-rdma/tests/test-in-docker-image.sh ${OS_TYPE} ${OS_VERSION} ${ANSIBLE_VERSION}"
